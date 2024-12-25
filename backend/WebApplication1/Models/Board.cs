476323a61810459ca1751a053f98923226b84999
Khoi <khoin147@gmail.com>
2024-12-25 17:32:49 +0700

    add datacontext and user interface

---
+++ backend/WebApplication1/Models/Board.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<BoardMember> BoardMembers { get; set; } = new List<BoardMember>();
+        public List<List> Lists { get; set; } = new List<List>();
+    }
+}
---
+++ backend/WebApplication1/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; }
+        public List<BoardMember> BoardMembers { get; set; } = new List<BoardMember>();
+        public List<CardAssignee> CardAssignees { get; set; } = new List<CardAssignee>();
+        public List<CardComment> CardComments { get; set; } = new List<CardComment> { };
+    }
+}
